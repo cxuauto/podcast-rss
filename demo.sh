d4e18a5a1da1de21cf121a14201ea4ed7f801a39
cxumol <cxumol@users.noreply.github.com>
2018-02-17 01:10:50 -0800

    edit:demo.sh

--- demo.sh
+++ demo.sh
@@ -4,4 +4,5 @@ ALBUMID="12825974"
 RSSFILE="2018sfcy.xml"
 # export PBUUID=""
 ./ximalaya_linux_amd64 $ALBUMID > $RSSFILE
-curl -X PUT -F c=@RSSFILE https://ptpb.pw/$PBUUID
+ls
+curl -X PUT -F c=@$RSSFILE https://ptpb.pw/$PBUUID
