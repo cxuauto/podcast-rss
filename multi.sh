46caa4fbfd2806c78c58fe7578b46e879a10afe0
cxumol <cxumol@users.noreply.github.com>
2018-02-21 04:05:36 -0800

    confusing bash array

--- multi.sh
+++ multi.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-ALBUMID=('2040956' '1837568' '1780124' )
-RSSFILE=('lgh.xml' '7890.xml' 'fger.xml')
+
+ALBUMID=([0]='2040956' [1]='1837568' [2]='1780124' )
+RSSFILE=([0]='lgh.xml' [1]='7890.xml' [2]='fger.xml')
 GRABBER="lizhi_linux_amd64"
 
-wget https://github.com/cxuauto/podcast-rss/releases/download/3jia/$GRABBER
+wget -q https://github.com/cxuauto/podcast-rss/releases/download/3jia/$GRABBER
 chmod +x ./$GRABBER
 
 len=${#ALBUMID[@]}
 
 for ((i=0;i<$len;i++));do
-        ./$GRABBER $ALBUMID[$i] > $RSSFILE[$i]
+        ./$GRABBER ${ALBUMID[$i]} > ${RSSFILE[$i]}
 done
 
 ls
 
 for ((i=0;i<$len;i++));do
-        curl -X PUT -F c=@$RSSFILE[i] https://ptpb.pw/$PBUUID$i
+        curl -X PUT -F c=@${RSSFILE[i]} https://ptpb.pw/$PBUUID$i
 done
